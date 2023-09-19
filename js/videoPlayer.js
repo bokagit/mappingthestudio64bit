5087647a1a34f9e6e888cbe9aae1c60c97a3b7fa
bokagit <43740696+bokagit@users.noreply.github.com>
2023-09-19 15:22:04 +0200

    all added

---
+++ js/videoPlayer.js
@@ -0,0 +1,13 @@
+var videoId = [	"rcm6kmLUggg", 
+			   	"4wb6o3QvrxY",
+			  	"IQIuVqt4sj8",
+			  	"1EiC9bvVGnk",
+			   	"P4tcVYgdgos"
+			  ];
+
+for (let x = 0; x < videoId.length; x++) {
+    var iframeId = "thumb" + (x+3);
+    document.getElementById(iframeId).src = "https://www.youtube.com/embed/" + videoId[x] + "?&autoplay=1";
+}
+
+document.getElementById("ls_embed_1573371168").src="https://livestream.com/accounts/4931571/events/5369913/player?";
