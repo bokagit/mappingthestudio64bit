771a9dc658b4c9ae44fd70f1a85a415ce158b9c0
bokagit <43740696+bokagit@users.noreply.github.com>
2025-08-19 11:08:55 +0200

    update-js-links

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,8 +1,8 @@
 var videoId = [	"KnlnB2Poxr0", 
 			   	"4wb6o3QvrxY",
-			  	"IQIuVqt4sj8",
+			  	"Zedu0ddRlDU",
 			  	"1EiC9bvVGnk",
-			   	"iWkGwIv20WI"
+			   	"AttVS4KM8tY"
 			  ];
 
 for (let x = 0; x < videoId.length; x++) {
@@ -10,4 +10,4 @@ for (let x = 0; x < videoId.length; x++) {
     document.getElementById(iframeId).src = "https://www.youtube.com/embed/" + videoId[x] + "?&autoplay=1";
 }
 
-document.getElementById("ls_embed_1573371168").src="https://livestream.com/accounts/4931571/events/5369913/player?";
+document.getElementById("ls_embed_1573371168").src="https://player.vimeo.com/video/1097394386?";
