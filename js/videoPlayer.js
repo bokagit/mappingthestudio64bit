4eba43b061bed4bbe1cacc7c165ad1c4c43c0d1e
bokagit <43740696+bokagit@users.noreply.github.com>
2025-09-08 21:33:01 +0200

    js

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -10,4 +10,4 @@ for (let x = 0; x < videoId.length; x++) {
     document.getElementById(iframeId).src = "https://www.youtube.com/embed/" + videoId[x] + "?&autoplay=1";
 }
 
-document.getElementById("ls_embed_1573371168").src="https://player.vimeo.com/video/1097394386?";
+document.getElementById("ls_embed_1573371168").src="https://vimeo.com/event/4721898/embed?title=0&byline=1&portrait=0&badge=0&background=1&muted=0";
